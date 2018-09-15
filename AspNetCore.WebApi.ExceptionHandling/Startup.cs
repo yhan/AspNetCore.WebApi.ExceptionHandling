d57ea278946dafb13cb939e1c84c0ffef9d105ed
Yi HAN <hanyi@8110@gmail.com>
2018-09-15 12:08:25 +0200

    bootstrap api

---
+++ AspNetCore.WebApi.ExceptionHandling/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AspNetCore.WebApi.ExceptionHandling
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(
+                swaggerGenOptions =>
+                {
+                    var webApiDescription = @"";
+                    swaggerGenOptions.SetDefaultOptionsWithXmlDocumentation(System.AppDomain.CurrentDomain.FriendlyName, "AM 10", "DIF-IT-LTAM10@lafrancaise-group.com", webApiDescription);
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "exception hadnling spike API V1"); });
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
+
---
+++ AspNetCore.WebApi.ExceptionHandling/SwagOptionsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AspNetCore.WebApi.ExceptionHandling
+{
+    public static class SwagOptionsExtensions
+    {
+        public static void SetDefaultOptionsWithXmlDocumentation(this SwaggerGenOptions options, string processName, string contactTeamName, string contactTeamEmail, string description)
+        {
+            options.IgnoreObsoleteActions();
+            options.IgnoreObsoleteProperties();
+            options.DescribeAllParametersInCamelCase();
+            options.DescribeAllEnumsAsStrings();
+            options.DescribeStringEnumsInCamelCase();
+            options.SwaggerDoc("v1", new Info()
+            {
+                Title = processName,
+                Version = "v1",
+                Contact = new Contact()
+                {
+                    Email = contactTeamEmail,
+                    Name = contactTeamName
+                },
+                TermsOfService = "Copyright, " + (object)DateTime.Now.Year + " La Française Asset Management, Inc. All Rights Reserved. For authorized use only.",
+                Description = description
+            });
+            options.IncludeXmlComments(GetXmlCommentsPath(processName), false);
+            //options.DocumentFilter<RemoveContextAttributesDocumentFilter>(Array.Empty<object>());
+            //options.DocumentFilter<RemoveAuthorizationControllerPathDocumentFilter>(Array.Empty<object>());
+            //options.OperationFilter<AddContextHeadersOperationFilter>(Array.Empty<object>());
+          
+        }
+
+        private static string GetXmlCommentsPath(string processName)
+        {
+            return string.Format("{0}\\{1}.XML", (object)AppDomain.CurrentDomain.BaseDirectory, (object)processName);
+        }
+    }
+}
