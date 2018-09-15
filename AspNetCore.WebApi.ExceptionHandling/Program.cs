08a5ee91381dc75252ecd2e339d23f991fc008fc
Yi HAN <hanyi@8110@gmail.com>
2018-09-15 12:26:16 +0200

    configure logger

--- AspNetCore.WebApi.ExceptionHandling/Program.cs
+++ AspNetCore.WebApi.ExceptionHandling/Program.cs
@@ -14,7 +14,26 @@ namespace AspNetCore.WebApi.ExceptionHandling
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+            webHost.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- AspNetCore.WebApi.ExceptionHandling/Startup.cs
+++ AspNetCore.WebApi.ExceptionHandling/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +17,7 @@ namespace AspNetCore.WebApi.ExceptionHandling
 {
     public class Startup
     {
+    
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -32,11 +36,24 @@ namespace AspNetCore.WebApi.ExceptionHandling
                     var webApiDescription = @"";
                     swaggerGenOptions.SetDefaultOptionsWithXmlDocumentation(System.AppDomain.CurrentDomain.FriendlyName, "AM 10", "DIF-IT-LTAM10@lafrancaise-group.com", webApiDescription);
                 });
+
+            //services.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
+            app.Use(next =>
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Incoming request");
+                    await next(context);
+                    logger.LogInformation("Outgoing response");
+                };
+            });
+
+            app.ConfigureExceptionHandler(logger);
 
             app.UseSwagger();
 
@@ -50,6 +67,39 @@ namespace AspNetCore.WebApi.ExceptionHandling
 
             app.UseMvc();
         }
+    }
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILogger logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new ErrorDetails
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = $"Internal Server Error: {contextFeature.Error}"
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
     }
 }
 
