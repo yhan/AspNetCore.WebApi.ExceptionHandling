ac7d23e2bf879dc172a6be3223dc942965a939a2
Yi HAN <hanyi@8110@gmail.com>
2018-09-30 13:14:32 +0200

    clean

--- AspNetCore.WebApi.ExceptionHandling/ApplicationBuilderExtensions.cs
+++ AspNetCore.WebApi.ExceptionHandling/ApplicationBuilderExtensions.cs
@@ -1,11 +1,9 @@
 ﻿namespace AspNetCore.WebApi.ExceptionHandling
 {
-    using System;
     using System.Text;
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Diagnostics;
-    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Configuration;
 
@@ -15,24 +13,20 @@
     {
         public static IApplicationBuilder UseCustomUnhandledExceptionHandler(this IApplicationBuilder app, string devTeamMailAddress, IConfiguration configuration, string applicationName)
         {
-            return app.UseExceptionHandler((Action<IApplicationBuilder>)(options => options.Run((RequestDelegate)RequestDelegate(devTeamMailAddress, applicationName))));
+            return app.UseExceptionHandler(applicationBuilder => applicationBuilder.Run(RequestDelegate()));
         }
 
-        private static RequestDelegate RequestDelegate(string devTeamMailAddress, string applicationName)
+        private static RequestDelegate RequestDelegate()
         {
             return async context =>
                 {
                     context.Response.StatusCode = 500;
                     context.Response.ContentType = "application/json";
                     IExceptionHandlerFeature exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    Guid guid = Guid.NewGuid();
 
                     string errorMessage = $"{exceptionHandlerFeature.Error}";
 
-                    await context.Response.WriteAsync(JsonConvert.SerializeObject((object)new
-                    {
-                        Message = errorMessage
-                    }, Formatting.Indented), Encoding.UTF8).ConfigureAwait(false);
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(new { Message = errorMessage }, Formatting.Indented), Encoding.UTF8).ConfigureAwait(false);
                 };
         }
     }
